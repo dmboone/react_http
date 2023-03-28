dd3ce849490da351f5782664804c439d80f2cbd3
Destiny Boone <destaj.db@gmail.com>
2023-03-28 14:28:18 -0400

    using async/await

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
 
-  function fetchMoviesHandler(){
+  async function fetchMoviesHandler() {
     // can optionally pass in and object to fetch {} that adds more specific configures for the request
-    fetch('https://swapi.dev/api/films')
-    .then((response) => {
-      return response.json(); // returns a promise
-    })
-    .then((data) => { // this grabs the response.json promise
-      const transformedMovies = data.results.map((movieData) => {
-        return{
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseData: movieData.release_date
-        }
-      });
-      setMovies(transformedMovies);
-    })
-  };
+    const response = await fetch("https://swapi.dev/api/films");
+    const data = await response.json();
+
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseData: movieData.release_date,
+      };
+    });
+    setMovies(transformedMovies);
+  }
 
   return (
     <React.Fragment>
